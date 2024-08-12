5d82ff314f074b5fc621b4db0e17f6e6297895bf
Joseph Muller <j.muller@bbk.ac.uk>
2024-08-12 14:30:53 +0100

    Migrate books docs from plugin repo #25

--- content/old-docs/use-dashes.py
+++ content/old-docs/use-dashes.py
@@ -2,9 +2,8 @@ import os
 
 directory = '/home/joemull/git/memory-alpha/content/old-docs'
 
-# Use underscore? Otherwise defaults to hyphen
-is_use_underscore = False
-char_to_use = '_' if is_use_underscore else '-'
+chars_to_replace = ['_', ' ']
+char_to_use = '-'
 
 print("Renaming files now!")
 for root, dirs, files in os.walk(directory):
@@ -13,14 +12,14 @@ for root, dirs, files in os.walk(directory):
     print(f"files: {files}")
 
     for current_filename in files:
-        new_filename = current_filename.replace('_', char_to_use)
-
-        print(f"current filename: {current_filename}")
-        print(f"    new filename: {new_filename}")
-
-        os.rename(
-            os.path.join(root, current_filename),
-            os.path.join(root, new_filename)
-        )
+        for char in chars_to_replace:
+            new_filename = current_filename.replace(char, char_to_use)
+            if current_filename != new_filename:
+                print(f"current filename: {current_filename}")
+                print(f"    new filename: {new_filename}")
+                os.rename(
+                    os.path.join(root, current_filename),
+                    os.path.join(root, new_filename)
+                )
 
 print("All done!")
