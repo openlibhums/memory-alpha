f9c4e7e63bc2210403a3bb319873c96bfbb6ffe2
Joseph Muller <j.muller@bbk.ac.uk>
2024-10-23 12:04:11 +0100

    Handle reduced motion preference more specifically #18

--- themes/alpha/static/components/george_space_splash.js
+++ themes/alpha/static/components/george_space_splash.js
@@ -1,6 +1,16 @@
 export function playPause() {
-  const buttons = document.querySelectorAll('.george-space-splash button');
+  const prefersReduced = window.matchMedia('(prefers-reduced-motion: reduce)');
   const animations = document.querySelectorAll('.animated');
+  const buttons = document.querySelectorAll('.george-space-splash button');
+
+  // Play animations when page loads only if user is ok with motion
+  for (const animation of Array.from(animations)) {
+    if (!prefersReduced.matches) {
+      animation.classList.toggle('paused');
+    }
+  }
+
+  // On click, change out the buttons and change the play state
   for (const button of Array.from(buttons)) {
     button.addEventListener('click', () => {
       for (const button of Array.from(buttons)) {
