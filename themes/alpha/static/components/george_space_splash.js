320cee5b6d10009db8ab0f8033de100a4771b309
Joseph Muller <j.muller@bbk.ac.uk>
2025-01-20 20:26:56 +0000

    chore #54 Run Prettier on JS

--- themes/alpha/static/components/george_space_splash.js
+++ themes/alpha/static/components/george_space_splash.js
@@ -1,39 +1,44 @@
 export function animateGeorge() {
-
-  const splash = document.querySelector('.george-space-splash');
-  const prefersReduced = window.matchMedia('(prefers-reduced-motion: reduce)');
+  const splash = document.querySelector(".george-space-splash");
+  const prefersReduced = window.matchMedia("(prefers-reduced-motion: reduce)");
   if (!splash) {
-    return
+    return;
   }
 
   // When page loads, play animations only if user is ok with motion
   if (!prefersReduced.matches) {
-    splash.classList.toggle('running');
+    splash.classList.toggle("running");
   }
 
   function playOrPause() {
-    splash.classList.toggle('running');
-    splash.classList.toggle('showing-slide-1', false);
-    splash.classList.toggle('showing-slide-2', false);
+    splash.classList.toggle("running");
+    splash.classList.toggle("showing-slide-1", false);
+    splash.classList.toggle("showing-slide-2", false);
   }
 
   // When user clicks play or pause, play or pause
-  const playPauseButton = document.querySelector('.george-space-splash .play-pause button');
-  playPauseButton.addEventListener('click', playOrPause);
+  const playPauseButton = document.querySelector(
+    ".george-space-splash .play-pause button",
+  );
+  playPauseButton.addEventListener("click", playOrPause);
 
   // When user selects slide 1, go to beginning of animation duration and pause
-  const selectSlide1 = document.querySelector('.george-space-splash .select-slide-1 button');
-  selectSlide1.addEventListener('click', () => {
-    splash.classList.toggle('running', false);
-    splash.classList.toggle('showing-slide-1', true);
-    splash.classList.toggle('showing-slide-2', false);
+  const selectSlide1 = document.querySelector(
+    ".george-space-splash .select-slide-1 button",
+  );
+  selectSlide1.addEventListener("click", () => {
+    splash.classList.toggle("running", false);
+    splash.classList.toggle("showing-slide-1", true);
+    splash.classList.toggle("showing-slide-2", false);
   });
 
   // When user selects slide 2, go to end of animation duration and pause
-  const selectSlide2 = document.querySelector('.george-space-splash .select-slide-2 button');
-  selectSlide2.addEventListener('click', () => {
-    splash.classList.toggle('running', false);
-    splash.classList.toggle('showing-slide-1', false);
-    splash.classList.toggle('showing-slide-2', true);
+  const selectSlide2 = document.querySelector(
+    ".george-space-splash .select-slide-2 button",
+  );
+  selectSlide2.addEventListener("click", () => {
+    splash.classList.toggle("running", false);
+    splash.classList.toggle("showing-slide-1", false);
+    splash.classList.toggle("showing-slide-2", true);
   });
 }
