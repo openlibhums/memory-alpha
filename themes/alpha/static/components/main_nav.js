742adf766cbefd1be823148fdff83fc4130263c7
Joseph Muller <j.muller@bbk.ac.uk>
2025-02-07 09:25:57 +0000

    chore #130 Continue fixing bad rebase

--- themes/alpha/static/components/main_nav.js
+++ themes/alpha/static/components/main_nav.js
@@ -2,7 +2,7 @@ export function slideDrawer() {
   const slideDrawer = document.getElementById("slide-drawer");
   const mainNav = document.getElementById("main-nav");
 
-  slideDrawer.addEventListener("click", function () {
-    mainNav.classList.toggle("open");
+  slideDrawer?.addEventListener("click", function () {
+    mainNav?.classList.toggle("open");
   });
 }
