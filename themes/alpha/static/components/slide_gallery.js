59632a4d636ea4ddce839ba2c31a4c169a2309d5
Joseph Muller <j.muller@bbk.ac.uk>
2026-04-29 13:55:15 +0100

    feat #184 Simplify slide gallery to use with quotes

--- themes/alpha/static/components/slide_gallery.js
+++ themes/alpha/static/components/slide_gallery.js
@@ -1,23 +1,16 @@
 function setArrowButton(buttonSelector, targetId, gallery) {
   const arrowButton = gallery.querySelector(buttonSelector);
   if (document.getElementById(targetId)) {
-    arrowButton?.setAttribute("data-target", targetId);
-    arrowButton?.classList.toggle("hidden", false);
+    arrowButton.setAttribute("data-target", targetId);
+    arrowButton.classList.toggle("hidden", false);
   } else {
-    arrowButton?.classList.toggle("hidden", true);
+    arrowButton.classList.toggle("hidden", true);
   }
 }
 
 function setSelection(targetId) {
-  const selectableButton = document.querySelector(
-    `.tablist-button[data-target="${targetId}"`,
-  );
-  const gallery = selectableButton?.closest("[data-slide-gallery]");
-  const tablistButtons = gallery?.querySelectorAll("[data-tablist-button]");
-  for (const otherButton of Array.from(tablistButtons)) {
-    otherButton.classList.toggle("selected", false);
-  }
-  selectableButton?.classList.toggle("selected", true);
+  const currentSlide = document.querySelector(`#${targetId}`);
+  const gallery = currentSlide.closest("[data-slide-gallery]");
 
   const previousNum = Number(targetId.slice(-1)) - 1;
   const previousTargetId = targetId.slice(0, -1) + previousNum;
@@ -31,7 +24,7 @@ function setSelection(targetId) {
 function setScrollDestination(button) {
   button.addEventListener("click", () => {
     const target = document.getElementById(button.getAttribute("data-target"));
-    target?.scrollIntoView({
+    target.scrollIntoView({
       block: "nearest",
     });
   });
@@ -45,7 +38,7 @@ const observerOptions = {
 function onIntersection(entries, _options) {
   for (const entry of entries) {
     if (entry.isIntersecting) {
-      setSelection(entry.target?.id);
+      setSelection(entry.target.id);
     }
   }
 }
@@ -55,13 +48,9 @@ const observer = new IntersectionObserver(onIntersection, observerOptions);
 export default function slideGallery() {
   const galleries = document.querySelectorAll("[data-slide-gallery]");
   for (const gallery of Array.from(galleries)) {
-    const slides = gallery.querySelectorAll("[data-current-slide]");
+    const slides = gallery.querySelectorAll("[data-slide]");
     for (const slide of Array.from(slides)) {
       observer.observe(slide);
-    }
-    const tablistButtons = gallery.querySelectorAll("[data-tablist-button]");
-    for (const button of Array.from(tablistButtons)) {
-      setScrollDestination(button);
     }
     const previousButton = gallery.querySelector("[data-previous-button]");
     setScrollDestination(previousButton);
