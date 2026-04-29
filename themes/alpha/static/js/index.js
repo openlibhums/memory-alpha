19966f59cb710d27e5d3b356b940dd410fc32903
Joseph Muller <j.muller@bbk.ac.uk>
2026-04-29 13:53:04 +0100

    feat #184 Simplify axiom slider as dot carousel

--- themes/alpha/static/js/index.js
+++ themes/alpha/static/js/index.js
@@ -1,9 +1,11 @@
-import slideGallery from "../components/slide_gallery.js";
-import { slideDrawer } from "../components/main_nav.js";
+import { dotCarousel } from "../components/dot_carousel.js";
 import { animateGeorge } from "../components/george_space_splash.js";
+import { slideDrawer } from "../components/main_nav.js";
+import slideGallery from "../components/slide_gallery.js";
 import timelineWheel from "../components/timeline_wheel.js";
 
+dotCarousel();
+animateGeorge();
 slideDrawer();
 slideGallery();
-animateGeorge();
 timelineWheel();
